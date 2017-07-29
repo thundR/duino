8beefb4729ada7b3d5784348cbf493c9a8db1274
thatmusicguy <eyoho12@gmail.com>
2017-07-28 23:32:51 -0700

    eee

---
+++ broke/broke.ino
@@ -0,0 +1,20 @@
+// Import required libraries
+#include "ESP8266WiFi.h"
+
+// WiFi parameters
+const char* ssid = "your_wifi_name";
+const cha
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected");
+
+// Print the IP address
+Serial.println(WiFi.localIP());
+
+}
+
+void loop() {
+
+}
+
